cdf9e8e78ce6ec2e465fba6c5bb0f9e55250045e
Tuğçe Eraslan <tugceeraslan34@gmail.com>
2022-07-21 16:37:31 +0300

    + FluentValidation Library

---
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Validations
+{
+    public class ProductDtoValidator: AbstractValidator<ProductDto>  // ProductDtoValidator sınıfımın FuluentValidation dan gelen sınıfı implemente etmemiz lazım o sınıfta AbstractValidator. Validator kullanacağım DTO da ProductDto
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName}  is required") ;
+
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertName} must be grater 0");  //price,double,datetime değer tipli olduklarından dolayı default değerleri var.O yüzden NotNull() ya da NotEmpty() yapmayız.
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertName} must be grater 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{ProperyName} must be grater 0");
+        }
+    }
+}
