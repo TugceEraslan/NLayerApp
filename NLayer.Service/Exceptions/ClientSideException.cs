2d1f6c341524d951722ccb82b2473797c995ecfd
Tuğçe Eraslan <tugceeraslan34@gmail.com>
2022-07-22 11:13:15 +0300

    + Global Exception Handler

---
+++ NLayer.Service/Exceptions/ClientSideException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Exceptions
+{
+    public class ClientSideException: Exception   // Hata class ı olduğu için Exception class ından dan miras alıyorum
+    {
+        public ClientSideException(string message): base(message) // base dediğim Exception ın ctor una gidiyor
+        {
+
+        }
+    }
+}
