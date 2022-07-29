c39cb53d2bbc24296802f6b95c4da5d8d7207038
Tuğçe Eraslan <tugceeraslan34@gmail.com>
2022-07-29 11:12:29 +0300

    + ProductsController Update-1

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -51,5 +51,36 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
             return View();  // Eğer başarısız ise category i tekrar yüklesin aynı sayfaya tekrar dönsün
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var product=await _services.GetByIdAsync(id);
+            // Drop DownList i doldurmam lazım
+            // Product ve Category ler arasında bire çok bir ilişki var
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());   // Neyi mapleyeceksem onu yazıyorum Map<List<CategoryDto>>(categories) şeklinde
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name",product.CategoryId);
+
+            return View(_mapper.Map<ProductDto>(product));
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.UpdateAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());   // Neyi mapleyeceksem onu yazıyorum Map<List<CategoryDto>>(categories) şeklinde
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
+            return View (productDto);  // productDto nesnemi döndüm
+
+        }
+
     }
 }
